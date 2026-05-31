b0c20d7aa070ad7987d6893cacb1ce83b86faa3e
OPPONGX3 <oppongprinceagyeman37@gmail.com>
2026-05-31 17:53:11 +0000

    Implement main function to print initials OPA
    Added a main function to print stylized initials OPA with specific formatting.

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Prints stylized large initials OPA with specific formatting constraints.
+# Assignment Number: 1
+#
+# Name: Oppong Prince Agyeman
+# STUDENT ID:  2425403304
+# Email: 2425403304@live.gctu.edu.gh
+# Grader: Prince
+#
+# On my honor, Oppong Prince Agyeman, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function handles the formatted printing of the small and stylized large initials.
+    print()
+    print("...OPA")
+    print()
+    print("...  OOOOOO  ......PPPPPPPP  ......    AA    ......    ** ...")
+    print("... OO    OO ......PP     PP ......   AAAA   ......    ** ...")
+    print("...OO      OO......PP     PP ......  AA  AA  ......    ** ...")
+    print("...OO      OO......PPPPPPPP  ...... AA    AA ......    ** ...")
+    print("...OO      OO......PP        ......AAAAAAAAAA......    ** ...")
+    print("...OO      OO......PP        ......AA      AA......    ** ...")
+    print("... OO    OO ......PP        ......AA      AA......    ** ...")
+    print("...  OOOOOO  ......PP        ......AA      AA......    ** ...")
+    print("...          ......          ......          ......  ****** ...")
+    print("...          ......          ......          ......  ****** ...")
+    print()
 
 
 main()
